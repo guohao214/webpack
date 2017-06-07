7bd16396ba162b3017dca098d8bad8514e823cbb
guohao <guohao214@gmail.com>
2017-06-07 23:02:29 +0800

    3

--- 01/app/view.js
+++ 01/app/view.js
@@ -2,11 +2,13 @@ import template from './template.html'
 
 console.log(template)
 
+alert(222333)
+
 require.ensure([], () => {
 	setTimeout(function() {
 		let body = document.getElementsByTagName('body')[0]
 		console.log(body)
 		body.innerHTML = template
-	}, 3000)
+	}, 0)
 })
 
